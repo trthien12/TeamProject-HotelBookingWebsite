3120bfd1ef1838be5b717c427fd0963dc9d2e12c
Ruby492 <030238220149@st.buh.edu.vn>
2025-04-03 01:47:50 +0700

    Điền Thong tin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\BookingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome'); // hoặc view trang chủ của bạn
+})->name('home');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/rooms', [PageController::class, 'rooms'])->name('rooms');
+Route::get('/pages', [PageController::class, 'pages'])->name('pages');
+Route::get('/news', [PageController::class, 'news'])->name('news');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+Route::get('/cart', [CartController::class, 'index'])->name('cart');
+
+Route::get('/dat-phong', [BookingController::class, 'showForm'])->name('booking.form');
+Route::post('/dat-phong', [BookingController::class, 'store'])->name('booking.store');
+
+Route::post('/booking-submit', [BookingController::class, 'submit'])->name('booking.submit');
+
+
+Route::post('/booking/submit', [BookingController::class, 'submitBooking'])->name('booking.submit');
+Route::get('/api/booking-info/{roomId}', [BookingController::class, 'getBookingInfo']);
