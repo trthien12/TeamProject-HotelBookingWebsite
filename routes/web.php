7aabbd730dd88c19b23117346204aa0d09048877
thnguyenn2001 <ng.thanhnguyen162@gmail.com>
2025-04-06 13:38:47 +0700

    Update trước khi đổi nhánh

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookingController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,22 +14,11 @@ use App\Http\Controllers\BookingController;
 */
 
 Route::get('/', function () {
-    return view('welcome'); // hoặc view trang chủ của bạn
-})->name('home');
+    return view('welcome');
+});
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/about', [PageController::class, 'about'])->name('about');
-Route::get('/rooms', [PageController::class, 'rooms'])->name('rooms');
-Route::get('/pages', [PageController::class, 'pages'])->name('pages');
-Route::get('/news', [PageController::class, 'news'])->name('news');
-Route::get('/contact', [PageController::class, 'contact'])->name('contact');
-Route::get('/cart', [CartController::class, 'index'])->name('cart');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('/dat-phong', [BookingController::class, 'showForm'])->name('booking.form');
-Route::post('/dat-phong', [BookingController::class, 'store'])->name('booking.store');
-
-Route::post('/booking-submit', [BookingController::class, 'submit'])->name('booking.submit');
-
-
-Route::post('/booking/submit', [BookingController::class, 'submitBooking'])->name('booking.submit');
-Route::get('/api/booking-info/{roomId}', [BookingController::class, 'getBookingInfo']);
+require __DIR__.'/auth.php';
