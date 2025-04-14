607c864e13c3c5451fa0e7c8c4023b1ae15a4384
thien <tngo2505@gmail.com>
2025-04-14 23:23:22 +0700

    xoá lần 2

--- resources/views/homepage/trangchu.blade.php
+++
@@ -1,8 +0,0 @@
-@extends("layouts.main")
-@section("title","Trang chủ Golden Tree")
-@section("content")
-    @include('homepage.sections.slider')
-    @include('homepage.sections.search')
-    @include('homepage.sections.about')
-    @include('homepage.sections.room_detail')
-@endsection
--- storage/framework/views/9e80b62b3a2b94cbcc0fa0cbf36bee80c5ab7bda.php
+++
@@ -1,9 +0,0 @@
-
-<?php $__env->startSection("title","Trang chủ Golden Tree"); ?>
-<?php $__env->startSection("content"); ?>
-    <?php echo $__env->make('homepage.sections.slider', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->make('homepage.sections.search', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->make('homepage.sections.about', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-    <?php echo $__env->make('homepage.sections.room_detail', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make("layouts.main", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Admin\Downloads\Baicua_\BTN-PTUDMNM-tcdt1\resources\views/homepage/trangchu.blade.php ENDPATH**/ ?>
